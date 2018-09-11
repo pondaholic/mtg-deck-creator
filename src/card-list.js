0143d2a885dc3fe29de26fb8e6d8f976a8edea0f
Shelly Leung <pontificable@yahoo.com>
2018-09-10 17:34:09 -0700

    fixed instances of super-nesting; trying to create each instance of card but it appears that the card names in the response are NOT unique must create an index for the cards or find unique property to become key

--- src/card-list.js
+++ src/card-list.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function CardList(props) {
-	console.log(props.cardList.cardList);
-	console.log(typeof props.cardList.cardList);
-	let newCardName = props.cardList.cardList.map(card => card.name);
+	console.log(props.cardList);
+	console.log(typeof props.cardList);
+	let newCard = props.cardList.map(card => (
+		<li key={card.name} value={card}>
+			Card
+		</li>
+	));
 	return (
 		<div className="card-list">
 			Cards:
-			{newCardName}
+			<ul>{newCard}</ul>
 		</div>
 	);
 }
 
 function mapStateToProps(state) {
-	// console.log(state);
-	// console.log(state.cardList);
-	return { cardList: state.cardList };
+	console.log(state.cards.cardList);
+	console.log(state);
+	return { cardList: state.cards.cardList };
 }
 
 const ConnectedCards = connect(mapStateToProps)(CardList);
--- src/store.js
+++ src/store.js
@@ -6,7 +6,8 @@ import thunk from 'redux-thunk';
 export default createStore(
 	combineReducers({
 		form: formReducer,
-		cardList: reducer,
+		cards: reducer,
 		middleware: applyMiddleware(thunk)
-	})
+	}),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
