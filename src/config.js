1b9e32d984b58ef33b590491d6448c51d7045da2
Shelly Leung <pontificable@yahoo.com>
2018-09-20 13:31:51 -0700

    edit config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	DATABASE_URL: process.env.DATABASE_URL || 'postgres://localhost/mtg-server',
+	// DATABASE_URL:
+	// 	process.env.DATABASE_URL ||
+	// 	'postgres://zoixupub:t_gexFN347TY-XDsTSZJMeL7XdtZOSgU@baasu.db.elephantsql.com:5432/zoixupub',
 	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 };
