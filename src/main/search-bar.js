7c1a1800a5a6829510b31016fc25f66e209b008e
Shelly Leung <pontificable@yahoo.com>
2018-10-14 22:00:12 -0700

    trying formik

--- src/main/search-bar.js
+++ src/main/search-bar.js
@@ -1,76 +1,60 @@
 import React from 'react';
-import Input from '../input';
-import { Field, reduxForm, focus } from 'redux-form';
-import { connect } from 'react-redux';
+import { Formik } from 'formik';
 
 //search "bar" should only be responsible for sending GET request
-export class SearchForm extends React.Component {
-	onSubmit(values) {
-		// event.preventDefault();
-		console.log(values);
-		// when user inputs search parameters into the form, the value in that input will be used to perform a GET request from the MTG API
-		// console.log(values);
-		// let searchTerm;
-		// let key;
-		// for (key in values) {
-		// 	if (values[key]) {
-		// 		if (
-		// 			values[key].toLowerCase() === 'blue' ||
-		// 			'red' ||
-		// 			'white' ||
-		// 			'black' ||
-		// 			'green'
-		// 		) {
-		// 			searchTerm = values[key];
-		// 		} else {
-		// 			searchTerm = encodeURIComponent(`%${values[key]}%`);
-		// 		}
-		// 	}
-		// }
-		// //using fetch request that will then only take the Name, Mana Cost, Color, Type, unique card ID, and text from the success
-		// console.log(key, searchTerm);
-		// this.props.dispatch(fetchCardsFromMtgApi(key, searchTerm)).then(() => {
-		// 	this.props.dispatch(reset('search'));
-		// });
-	}
-	render() {
-		return (
-			<div className="search-form">
-				<form onSubmit={this.handleSubmit(event => this.onSubmit(event))}>
-					<Field
-						label="Name of Card"
-						name="name"
-						id="name"
-						type="text"
-						component={Input}
+export default function Searchbar(props) {
+	return (
+		<Formik
+			initialValues={{ email: '', password: '' }}
+			validate={values => {
+				let errors = {};
+				if (!values.email) {
+					errors.email = 'Required';
+				} else if (
+					!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+				) {
+					errors.email = 'Invalid email address';
+				}
+				return errors;
+			}}
+			onSubmit={(values, { setSubmitting }) => {
+				setTimeout(() => {
+					alert(JSON.stringify(values, null, 2));
+					setSubmitting(false);
+				}, 400);
+			}}
+		>
+			{({
+				values,
+				errors,
+				touched,
+				handleChange,
+				handleBlur,
+				handleSubmit,
+				isSubmitting
+			}) => (
+				<form onSubmit={handleSubmit}>
+					<input
+						type="email"
+						name="email"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.email}
 					/>
-					<Field
-						label="Color of Card"
-						name="colors"
-						id="colors"
-						type="text"
-						component={Input}
+					{errors.email && touched.email && errors.email}
+					<input
+						type="password"
+						name="password"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.password}
 					/>
-					<Field
-						label="Type of Card"
-						name="type"
-						id="type"
-						type="text"
-						component={Input}
-					/>
-					<button type="submit" disabled={this.pristine || this.submitting}>
+					{errors.password && touched.password && errors.password}
+					<button type="submit" disabled={isSubmitting}>
 						Submit
 					</button>
 				</form>
-			</div>
-		);
-	}
+			)}
+		</Formik>
+	);
 }
-
-SearchForm = connect()(SearchForm);
-
-export default reduxForm({
-	form: 'search',
-	onSubmitFail: (errors, dispatch) =>
-		dispatch(focus('search', Object.keys(errors)[0]))
-})(SearchForm);
