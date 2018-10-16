6abae140e5458fe4ff78096bb1b76065066e0f24
Shelly Leung <pontificable@yahoo.com>
2018-10-15 22:25:58 -0700

    refactored out searchform from main component

--- src/main/search-bar.js
+++ src/main/search-bar.js
@@ -3,40 +3,17 @@ import Input from '../input';
 import { Field, reduxForm, focus } from 'redux-form';
 import { connect } from 'react-redux';
 
+import { fetchCardsFromMtgApi } from '../actions';
+
 //search "bar" should only be responsible for sending GET request
 export class SearchForm extends React.Component {
 	onSubmit(values) {
-		// event.preventDefault();
 		console.log(values);
-		// when user inputs search parameters into the form, the value in that input will be used to perform a GET request from the MTG API
-		// console.log(values);
-		// let searchTerm;
-		// let key;
-		// for (key in values) {
-		// 	if (values[key]) {
-		// 		if (
-		// 			values[key].toLowerCase() === 'blue' ||
-		// 			'red' ||
-		// 			'white' ||
-		// 			'black' ||
-		// 			'green'
-		// 		) {
-		// 			searchTerm = values[key];
-		// 		} else {
-		// 			searchTerm = encodeURIComponent(`%${values[key]}%`);
-		// 		}
-		// 	}
-		// }
-		// //using fetch request that will then only take the Name, Mana Cost, Color, Type, unique card ID, and text from the success
-		// console.log(key, searchTerm);
-		// this.props.dispatch(fetchCardsFromMtgApi(key, searchTerm)).then(() => {
-		// 	this.props.dispatch(reset('search'));
-		// });
 	}
 	render() {
 		return (
 			<div className="search-form">
-				<form onSubmit={this.handleSubmit(event => this.onSubmit(event))}>
+				<form onSubmit={this.props.handleSubmit(event => this.onSubmit(event))}>
 					<Field
 						label="Name of Card"
 						name="name"
